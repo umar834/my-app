460e875e54fd36f15a4d98662025f54ba97a567e
umar834 <63906422+umar834@users.noreply.github.com>
2020-06-09 13:46:51 +0500

    class 3 assignment

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World From Umar</h1>
-    </div>
-  );
-}
+const App = ({name}) => <div className="App">
+      <h1>Hello <strong>{name}</strong></h1>
+      <h2>This is class 3 Assignment</h2>
+      <div>
+        5 + 3 = {5 + 3}
+      </div>
+    </div>;
 
 export default App;
